d620b3f35b3eec2d5130eaf685dc02f470e02df8
ITLAB <ITLAB@IT>
2017-02-07 14:33:53 +0530

    third commit

--- src/com/example/basicgraphics/MainActivity.java
+++ src/com/example/basicgraphics/MainActivity.java
@@ -1,35 +1,70 @@
-package com.example.basicgraphics;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-}
+package com.example.basicgraphics;  
+  
+import android.os.Bundle;  
+import android.app.Activity;  
+import android.view.Menu;  
+import android.content.Context;  
+import android.graphics.Canvas;  
+import android.graphics.Color;  
+import android.graphics.Paint;  
+import android.view.View;  
+  
+public class MainActivity extends Activity {  
+  
+    DemoView demoview;  
+    /** Called when the activity is first created. */  
+    @Override  
+    public void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        demoview = new DemoView(this);  
+        setContentView(demoview);  
+    }  
+  
+    private class DemoView extends View{  
+        public DemoView(Context context){  
+            super(context);  
+        }  
+  
+        @Override protected void onDraw(Canvas canvas) {  
+            super.onDraw(canvas);  
+  
+            // custom drawing code here  
+            Paint paint = new Paint();  
+            paint.setStyle(Paint.Style.FILL);  
+  
+            // make the entire canvas white  
+            paint.setColor(Color.WHITE);  
+            canvas.drawPaint(paint);  
+              
+            // draw blue circle with anti aliasing turned off  
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.BLUE);  
+            canvas.drawCircle(20, 20, 15, paint);  
+  
+            // draw green circle with anti aliasing turned on  
+            paint.setAntiAlias(true);  
+            paint.setColor(Color.GREEN);  
+            canvas.drawCircle(60, 20, 15, paint);  
+  
+            // draw red rectangle with anti aliasing turned off  
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.RED);  
+            canvas.drawRect(100, 5, 200, 30, paint);  
+                           
+            // draw the rotated text  
+            canvas.rotate(-45);  
+                      
+            paint.setStyle(Paint.Style.FILL);  
+            canvas.drawText("Graphics Rotation", 40, 180, paint);  
+              
+            //undo the rotate  
+            canvas.restore();  
+        }  
+    }  
+    @Override  
+    public boolean onCreateOptionsMenu(Menu menu) {  
+        // Inflate the menu; this adds items to the action bar if it is present.  
+        getMenuInflater().inflate(R.menu.main, menu);  
+        return true;  
+    }  
+}  
